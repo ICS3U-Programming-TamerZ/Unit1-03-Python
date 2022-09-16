01d58f8794acaa7b834f462ccf37c6746712c46e
Tamer Zreg <tamer.zreg@stu.oscb.ca>
2022-09-16 03:14:41 +0000

    Added hello_world_style.py

---
+++ hello_world_style.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# Created By: Tamer Zreg
+# Date: 9 15, 2022
+# This Program shows Hello World with proper style.
+
+
+def main ():
+    print("Hello, World! With proper style!")
+
+
+if __name__ == "__main__":
+    main()
